7311513c6cce0645c73a7426dd1c2acd0bd80237
Martinesko <Martinesko@MARTINESKO>
2022-09-13 20:43:49 +0300

    all good

--- JS/JS Advanced/Syntax, Functions and Statements - Exercise/Fruit.js
+++ JS/JS Advanced/Syntax, Functions and Statements - Exercise/Fruit.js
@@ -1,3 +1,5 @@
-function fruit(name,number,grams) {
-
+function fruit(name,grams,money) {
+    let kg = grams/1000
+console.log(`I need $${(money*kg).toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${name}.`)
 }
+fruit('orange' ,2500, 1.80)
