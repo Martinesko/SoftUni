156bade1a2137634569d25cdaf543def82df75af
Martinesko <m.a.enev06@gmail.com>
2022-09-23 19:52:24 +0300

    New Project

---
+++ JS/JS Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -0,0 +1,7 @@
+function editElement(ref , match , rep) {
+   const content = ref.textContent;
+   const matcher = new RegExp(match,'g');
+   const edited = content.replace(matcher, rep);
+   ref.textContent = edited;
+
+}
