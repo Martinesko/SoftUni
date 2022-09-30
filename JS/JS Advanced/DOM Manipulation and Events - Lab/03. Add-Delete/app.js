fb00c55c17162cd33c8f7f41356758eafb3a6063
Martinesko <m.a.enev06@gmail.com>
2022-10-01 00:43:31 +0300

    New project

--- JS/JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,9 +1,16 @@
 function addItem() {
-    function addItem() {
-        function addItem() {
             let items = document.getElementById('items');
             let textboxvalue = document.getElementById('newItemText').value;
-            items.innerHTML += '<li>'+ textboxvalue +'</li>';
-        }
-    }
+            let deleteButton = document.createElement('a');
+
+            let li = document.createElement('li');
+            li.textContent =  textboxvalue;
+
+            deleteButton.href = "#";
+            deleteButton.textContent = '[Delete]';
+
+
+            deleteButton.addEventListener('click',function (event){event.target.parentElement.remove()});
+            li.appendChild(deleteButton);
+            items.appendChild(li);
 }
--- JS/JS Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
-function attachGradientEvents() {
-    console.log('TODO:...');
-}
+    function attachGradientEvents() {
+      let grad = document.getElementById('gradient');
+      let result = document.getElementById('result');
+
+      grad.addEventListener('mousemove',  (e) =>{
+    let maxWidth = e.target.clientWidth;
+    let pointerpostionx = e.offsetX;
+    let percentage = Math.floor((pointerpostionx / maxWidth)*100);
+    result.textContent = `${percentage}%`;
+      }
+    )
+    }
