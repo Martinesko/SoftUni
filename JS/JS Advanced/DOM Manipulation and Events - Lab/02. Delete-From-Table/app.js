e4a9d364638272f967b3364350538cd7728a3041
Martinesko <m.a.enev06@gmail.com>
2022-09-30 23:23:22 +0300

    New project

--- JS/JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let textBoxValue = document.getElementsByName('email')[0].value;
+    let customers = document.getElementById('customers').getElementsByTagName('td');
+    let flag = false;
+    for (let customerEmail of customers) {
+        if(customerEmail.textContent === textBoxValue){
+            customerEmail.parentElement.remove();
+            flag = true;
+        }
+
+            let result = document.getElementById('result');
+            result.textContent = flag? 'Deleted' : 'Not found.';
+
+    }
 }
--- JS/JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    //TODO...
+    function addItem() {
+        function addItem() {
+            let items = document.getElementById('items');
+            let textboxvalue = document.getElementById('newItemText').value;
+            items.innerHTML += '<li>'+ textboxvalue +'</li>';
+        }
+    }
 }
