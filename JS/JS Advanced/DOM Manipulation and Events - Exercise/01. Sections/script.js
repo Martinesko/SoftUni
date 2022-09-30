fe216d65cbdae24a156610d12a065e9413e3a267
Martinesko <m.a.enev06@gmail.com>
2022-10-01 00:54:30 +0300

    New project

--- JS/JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS/JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+  let result = document.getElementById('content');
+  for (let word of words) {
+     let div = document.createElement('div');
+     let p = document.createElement('p');
+     p.style.display = 'none';
+     div.appendChild(p);
+     div.addEventListener('click' , function (event){
+        p.style.display = 'block';
+     } )
+     result.appendChild(div);
+  }
+
+
 }
