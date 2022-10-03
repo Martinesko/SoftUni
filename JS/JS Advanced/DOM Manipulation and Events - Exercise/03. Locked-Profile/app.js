18abb6a702154bae7c6be44411cd774a25db48a2
Martinesko <m.a.enev06@gmail.com>
2022-10-03 20:47:28 +0300

    New project

--- JS/JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.getElementsByTagName('button');
+    for (const button of buttons) {
+        button.addEventListener('click', function (){
+            let person = button.parentElement;
+                let lock = person.children.item(4);
+            let information = person.children.item(9);
+            if (lock.checked == true){
+                if (button.textContent == 'Show more'){
+                    button.textContent = 'Hide it';
+                    information.style.display = 'block';
+                }
+                else if (button.textContent == 'Hide it'){
+                    information.style.display = 'none';
+                    button.textContent == 'Show more';
+                }
+            }
+            else{
+                button.disabled = false;
+            }
+
+        })
+    }
+
+
 }
--- JS/JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let inputTextArea = document.querySelector('textarea');
+    let ButtonOne = document.querySelector('button');
+
+    let outputTextArea = document.querySelectorAll('textarea')[1];
+    let ButtonTwo = document.querySelectorAll('button')[1];
+    ButtonOne.addEventListener('click', function (){
+        let startingText = inputTextArea.value;
+        let outputText = '';
+        for (let i = 0; i < startingText.length; i++) {
+            let encodedLetter = String.fromCharCode(startingText.charCodeAt(i) + 1);
+            outputText += encodedLetter;
+        }
+        inputTextArea.value = '';
+        outputTextArea.value = outputText;
+    });
+    ButtonTwo.addEventListener('click', function (){
+        let startingText = outputTextArea.value;
+        let outputText = '';
+        for (let i = 0; i < startingText.length; i++) {
+            let encodedLetter = String.fromCharCode(startingText.charCodeAt(i) - 1);
+            outputText += encodedLetter;
+        }
+        inputTextArea.value = outputText;
+    });
 }
