9cd12aa3f4625722218b38fe0bd13d9aeb5b8172
Martinesko <m.a.enev06@gmail.com>
2022-10-08 19:54:40 +0300

    New project

--- JS/JS Advanced/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ JS/JS Advanced/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -5,8 +5,7 @@ function solve() {
         let products = JSON.parse(document.getElementsByTagName('textarea')[0].value);
         for (const product of products) {
             let tr = document.createElement('tr');
-            tr.innerHTML = `<td><img src="${product.img}"> </td> <td> <p>${product.name}</p></td><td><p>${product.price}</p></td><td><p>${product.decFactor}
-              </p></td><td><input type="checkbox"/> </td>`;
+            tr.innerHTML = `<td><img src="${product.img}"><td><p>${product.name}</p><td><p>${product.price}</p><td><p>${product.decFactor}</p><td><input type="checkbox"/></td>`;
             list.appendChild(tr);
             document.getElementsByTagName('textarea')[0].value = '';
         }
@@ -16,22 +15,23 @@ function solve() {
     sellButton.addEventListener('click',function () {
         let bought = [];
         for (const product of Array.from(list.children)) {
-            if (product.getElementsByTagName("input")[0].checked == true){
+            if (product.getElementsByTagName('input')[0].checked == true){
                 let info = product.querySelectorAll('p');
                 let name = info[0].textContent;
-                let price = info[1].textContent;
-                let decFactor = info[2].textContent;
+                let price = Number(info[1].textContent);
+                let decFactor = Number(info[2].textContent);
                 bought.push({
                          name: name,
                          price : price,
                         decFactor : decFactor
-                }
-                );
+                });
             }
         }
+        let output = '';
             let outputTextBox = document.getElementsByTagName('textarea')[1];
-            outputTextBox.value = `{Bought furniture: ${bought.map(e => e.name(", "))}`;
-            outputTextBox.value += `\nTotal price ${bought.reduce((total, current) => total + current.price, 0).toFixed(2)}`;
-            outputTextBox.value += `\nAverage decoration factor: ${bought.reduce((total, current) => total + current.decFactor, 0) / Bought.length}`;
+        output = `Bought furniture: ${bought.map(e => e.name).join(", ")}`;
+        output += `\nTotal price: ${bought.reduce((total, current) => total + current.price, 0).toFixed(2)}`;
+        output += `\nAverage decoration factor: ${bought.reduce((total, current) => total + current.decFactor, 0) / bought.length}`;
+         outputTextBox.value = output;
     })
 }
--- JS/JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS/JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    console.log('TODO:...');
+   let maindiv = document.getElementsByTagName('div')[0];
+   Array.from(maindiv.getElementsByTagName("input")).forEach(element => {
+       element.addEventListener("focus", function(e){
+         e.target.parentNode.classList.add("focused");
+
+         e.target.addEventListener('blur',function (){
+          e.target.parentNode.classList.remove("focused");
+         })
+      });
+   });
 }
