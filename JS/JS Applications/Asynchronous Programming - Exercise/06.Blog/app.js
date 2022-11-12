4b5be59c405f84932772cf16d268046e7a46cbeb
Martinesko <m.e.anev06@gmail.com>
2022-11-12 14:02:36 +0200

    hi

--- JS/JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
+++ JS/JS Applications/Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,19 +1,33 @@
 function attachEvents() {
 
    let loadPosts = document.getElementById('btnLoadPosts');
+   let viewButton = document.getElementById('btnViewPost');
    let posts = document.getElementById('posts');
 
    loadPosts.addEventListener('click', async function(){
        let postsUrl = `http://localhost:3030/jsonstore/blog/posts`;
        let postsResponse = await fetch(postsUrl);
        let postsData = await postsResponse.json();
-       Object.entries(postsData).forEach([])
-       for (const postData of postsData) {
-           let option = document.createElement('option');
-           option.textContent = postData.title;
-           option.value = postData.data;
-            posts.appendChild(option);
-       }
+       let title = "";
+       let body = "";
+       let postId = "";
+
+           Object.entries(postsData).forEach(([code,data])=> {
+
+               posts.innerHTML+= `<option value =${code}>${data.title}</option>`;
+
+           })
+       viewButton.addEventListener('click',()=>{
+           Object.entries(postsData).forEach(([code,data])=> {
+            if(posts.value === code){
+                title = data.title;
+                body = data.body;
+                postId = data.postId;
+            }
+           })
+
+
+       })
    })
 }
 attachEvents();
